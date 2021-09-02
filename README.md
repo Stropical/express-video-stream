d2a1ff7c159902903e4edd4252a55545e0e4933a
Stropical <ethan@marreel.com>
2021-09-02 14:39:44 -0700

    Added the addVideo function

--- README.md
+++ README.md
@@ -14,16 +14,17 @@ The first step is importing it.
 const evs = require('express-video-stream') // Express Video Stream
 ```
 
-Once you have created your express app, this package needs 3 lines to work. The first line sets the config. The config is where you set your video ID and path behind it. This is so you can ask the server for a video without needing the path to the file. The config should contain an array named files with objects that have an id and path. You can also use the addVideo function, which takes the id then the path.
+Once you have created your express app, this package needs 2 lines to work. The first line sets the config. The config is where you set your video ID and path behind it. This is so you can ask the server for a video without needing the path to the file. The config should contain an array named files with objects that have an id and path. You can also use the addVideo function, which takes the id then the path.
 ```javascript
 evs.setConfig(JSON.parse(fs.readFileSync('./sample/config.json'))); //Load config from file
+// or
 evs.addVideo("Demo3", "./sample/vids/test3.mp4")    //Add video to config
 ```
 The last one takes the EVS object you’ve created and sets it as middleware for express.
 ```javascript
 app.use(evs.middleware) //Use streaming middleware
 ```
-Once your backend is set up its time to look at front end. All you need is a video tag with a source tag inside. The source’s src tag should be ‘/vidChunk?id=<put your id here>’. The server will return a 404 if the video’s ID cannot be found.
+Once your backend is set up its time to look at front end. All you need is a video tag with a source tag inside. The source’s src tag should be ‘/vidChunk?id={put your id here}. The server will return a 404 if the video’s ID cannot be found.
 ```html
 <video id="videoPlayer" width="600" controls muted="muted" autoplay>
   <source src="/vidChunk?id=test3" type="video/mp4" />
--- middleware.js
+++ middleware.js
@@ -15,24 +15,36 @@ function updateFileSizes() { // Searches for each file path and gets the size an
             } catch {
                 console.error("[STREAM] NO FILE FOUND AT: " + file.path)
             }}
-    )}
+    )
+}
+
+function addVideo(id, path) {
+    if(config.files) {  //Check if config.files exists
+        config.files.push({id: id, path: path})
+    } else {    
+        config.files = [];  //If config is empty create empty array and push
+        config.files.push({id: id, path: path})
+    }
+}
 
 function middleware(req, res, next) {
-    
     if (req.path == "/getIDs") { // Return array of all the video IDs
         var result = [];
         config.files.forEach((obj) => result.push(obj.id))
         res.status(200).send(result);
+        next();
     } else if (req.path == "/vidChunk") {
         const range = req.headers.range;    // Get range of data needed
         if (!req.query.id && range) {
             res.status(400).send("No video ID specified or no range headers")
+            next();
         } else {    // Make sure ID is specified
             var found = false;
             config.files.forEach((entry) => {
                 if (entry.id == req.query.id && entry.size) { // ID has been found and size is defined
                     found = true;
-
+                    updateFileSizes();
+                    
                     const CHUNK_SIZE = 10 ** 6; // Parse Range
                     const start = Number(range.replace(/\D/g, ""));
                     const end = Math.min(start + CHUNK_SIZE, entry.size - 1);
@@ -48,6 +60,8 @@ function middleware(req, res, next) {
                     res.writeHead(206, headers);    //Write headers then pipe video data to response
                     var vs = fs.createReadStream(path.resolve(entry.path), {start, end})
                     vs.pipe(res);
+
+                    
                     next();
                 }
             })
@@ -56,10 +70,11 @@ function middleware(req, res, next) {
                 res.status(404).send("No video found")
                 next();
             }
-
+            updateFileSizes();
             next();
         }
     } else {
+        updateFileSizes();
         next();
     }
 }
@@ -68,5 +83,5 @@ function middleware(req, res, next) {
 module.exports = {
     middleware,
     setConfig,
-    updateFileSizes
+    addVideo
 }
